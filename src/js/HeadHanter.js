9c7add2df8b18ce5bfbf2e58df496baca48e7fa6
dubrovinmikhail <mail@mail.com>
2023-06-23 15:36:59 +0300

    fixed randomWindow

--- src/js/HeadHanter.js
+++ src/js/HeadHanter.js
@@ -3,12 +3,12 @@ export default class HeadHanter {
     const randindex = Math.floor(Math.random() * (windowsList.length - 1));
     if (windowsList[0].parentElement.querySelector(".head")) {
       const headindex = Array.from(windowsList).findIndex((element) => element.classList.contains("head"))
-      console.log(headindex)
       windowsList[headindex].classList.remove("head");
       let randindex = Math.floor(Math.random() * (windowsList.length - 1));
         while (randindex == headindex) {
           randindex = Math.floor(Math.random() * (windowsList.length - 1));
-        } 
+        }
+        windowsList[randindex].classList.add("head"); 
     } else {
       windowsList[randindex].classList.add("head");
     }
