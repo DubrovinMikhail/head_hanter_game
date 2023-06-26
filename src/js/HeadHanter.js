76752310f9da41a555d21e2007fb2ce07cf813d5
dubrovinmikhail <mail@mail.com>
2023-06-26 22:21:46 +0300

    add github direktory

--- src/js/HeadHanter.js
+++ src/js/HeadHanter.js
@@ -2,13 +2,15 @@ export default class HeadHanter {
   randomWindow(windowsList) {
     const randindex = Math.floor(Math.random() * (windowsList.length - 1));
     if (windowsList[0].parentElement.querySelector(".head")) {
-      const headindex = Array.from(windowsList).findIndex((element) => element.classList.contains("head"))
+      const headindex = Array.from(windowsList).findIndex((element) =>
+        element.classList.contains("head")
+      );
       windowsList[headindex].classList.remove("head");
       let randindex = Math.floor(Math.random() * (windowsList.length - 1));
-        while (randindex == headindex) {
-          randindex = Math.floor(Math.random() * (windowsList.length - 1));
-        }
-        windowsList[randindex].classList.add("head"); 
+      while (randindex == headindex) {
+        randindex = Math.floor(Math.random() * (windowsList.length - 1));
+      }
+      windowsList[randindex].classList.add("head");
     } else {
       windowsList[randindex].classList.add("head");
     }
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-import HeadHanter from "./HeadHanter.js"; 
+import HeadHanter from "./HeadHanter.js";
 const windows = document.querySelectorAll(".window");
 document.addEventListener("DOMContentLoaded", () => {
   const headChange = setInterval(() => {
-    HeadHanter.prototype.randomWindow(windows)
+    HeadHanter.prototype.randomWindow(windows);
   }, 1000);
 });
