df3058b23a00a98a2880951651eab4e6560c732a
dubrovinmikhail <mail@mail.com>
2023-08-23 09:44:09 +0300

    attempt to write class for the cursor

--- src/components/cursor/cursor.js
+++ src/components/cursor/cursor.js
@@ -4,8 +4,18 @@ export class Cursor {
   constructor (element) {
     if (typeof element === "string") {
       element = document.querySelector(element);
-    }
-    
+    };
+    let x = element.clientX;
+    let y = element.clientY;
+    this.style.left = x + "px";
+    this.style.top = y + "px";
+  };
 
-  }
+  mouseBtnDown() {
+    this.children[0].classList.add("cursorDown");
+  };
+
+  mouseBtnUp() {
+  this.children[0].classList.remove("cursorDown");
+  };
 }
---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+import "../css/style.css";
+import { Cursor } from "../components/cursor/cursor";
+
+const table = document.querySelector(".table");
+const cells = table.children;
+const countField = document.querySelector(".count");
+
+const getRandom = () => Math.floor(1 + Math.random() * 15);
+
+let countStep = 0;
+let countClick = 0
+let lastTarget = getRandom();
+
+const removeActiveByIndex = (index) => cells[index].classList.remove("active");
+const appendActiveByIndex = (index) => cells[index].classList.add("active");
+
+
+const headChange = () => {
+      removeActiveByIndex(lastTarget);
+      lastTarget = getRandom();
+      appendActiveByIndex(lastTarget);
+      countField.textContent =  `количество пропусков: ${countStep - countClick}`;
+      countStep += 1;
+      if (countStep < countClick + 6) {
+        timeout = setTimeout(headChange, 1000);
+      } else {
+        table.removeEventListener('click', headClick);
+        countField.textContent = 'Стоп игра. Вы проиграли'
+      }
+};
+
+let timeout = setTimeout(headChange, 1000);
+
+const headClick = ({target}) => { 
+  if (target.classList.contains("active")) {
+    target.classList.remove("active")
+    countClick += 1;
+    clearTimeout(timeout);
+    timeout = setTimeout(headChange, 1000);
+  };
+}
+
+table.addEventListener('click', headClick);
+
+const cursor = new Cursor(".cursor")
+document.addEventListener('mousedown', cursor.mouseBtnDown())
+document.addEventListener('mouseup', cursor.mouseBtnUp())
+
+// const cursor = document.querySelector(".cursor")
+// const mouseMove = (element) => {
+//   let x = element.clientX;
+//   let y = element.clientY;
+//   cursor.style.left = x + "px";
+//   cursor.style.top = y + "px";
+// };
+
+// document.addEventListener('mousemove', mouseMove);
+
+// document.addEventListener('mousedown', () => {cursor.children[0].classList.add("cursorDown")})
+// document.addEventListener('mouseup', () => {cursor.children[0].classList.remove("cursorDown")})
