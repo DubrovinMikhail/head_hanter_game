41f95d5e5f1befbf8251dc68137df855f8e7f9d5
dubrovinmikhail <mail@mail.com>
2023-09-15 22:24:08 +0300

    end fix

--- src/components/headHanterGame/headHanterGame.js
+++ src/components/headHanterGame/headHanterGame.js
@@ -1,27 +1,23 @@
 import "./headHunterGame.css";
 
 export class HeadHunter {
-  static countStep = 0;
-  static countClick = 0;
-
   static getRandom() {
-    return Math.floor(1 + Math.random() * 15); 
-   }
+    return Math.floor(1 + Math.random() * 15);
+  }
 
   constructor(element) {
     if (typeof element === "string") {
       element = document.querySelector(element);
     }
     this.cells = element.children;
-    this.countField = element.querySelector(".count") 
+    this.countField = element.querySelector(".count");
   }
 
   removeActiveByIndex(index) {
     this.cells[index].classList.remove("active");
-  };
+  }
 
   appendActiveByIndex(index) {
     this.cells[index].classList.add("active");
-  };
+  }
 }
-
