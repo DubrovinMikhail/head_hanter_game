256815a35419636661b31e14e045367772c41e3e
dubrovinmikhail <mail@mail.com>
2023-09-15 11:11:28 +0300

    fixed style.css

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,36 @@
-import "../css/style.css";
 import { HeadHunter } from "../components/headHanterGame/headHanterGame";
 
-const table = new HeadHunter(".table")
+const table = new HeadHunter(".table");
 
 let countStep = 0;
-let countClick = 0
+let countClick = 0;
 let lastTarget = HeadHunter.getRandom();
 
 const headChange = () => {
   table.removeActiveByIndex(lastTarget);
   lastTarget = HeadHunter.getRandom();
   table.appendActiveByIndex(lastTarget);
-  table.countField.textContent =  `количество пропусков: ${countStep - countClick}`;
+  table.countField.textContent = `количество пропусков: ${
+    countStep - countClick
+  }`;
   countStep += 1;
-    if (countStep < countClick + 6) {
-      timeout = setTimeout(headChange, 1000);
-    } else {
-      table.cells[0].parentNode.removeEventListener('click', headClick);
-      table.countField.textContent = 'Стоп игра. Вы проиграли'
-    }
+  if (countStep < countClick + 6) {
+    timeout = setTimeout(headChange, 1000);
+  } else {
+    table.cells[0].parentNode.removeEventListener("click", headClick);
+    table.countField.textContent = "Стоп игра. Вы проиграли";
+  }
 };
 
 let timeout = setTimeout(headChange, 1000);
 
-const headClick = ({target}) => { 
+const headClick = ({ target }) => {
   if (target.classList.contains("active")) {
-    target.classList.remove("active")
+    target.classList.remove("active");
     countClick += 1;
     clearTimeout(timeout);
     timeout = setTimeout(headChange, 1000);
-  };
-}
+  }
+};
 
-table.cells[0].parentNode.addEventListener('click', headClick);
+table.cells[0].parentNode.addEventListener("click", headClick);
